c9442eaa8d50c749c038a1aab22713142068149e
Ioannis Tsanaktsidis <tsanakts.10@gmail.com>
2024-10-09 16:40:16 +0200

    ui: introduces react app with antd
    * Fetches and displays data from the django app
    
    * Adds tests

---
+++ ui/src/NoResultsMessage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+type NoResultsMessageProps = {
+    show: boolean;
+}
+
+export const NoResultsMessage: React.FC<NoResultsMessageProps> = ({show}) => {
+    if (show) {
+        return <div style={{textAlign: 'center', marginTop: '20px'}}>No results found. Please refine your query!</div>;
+    }
+    return null;
+}
---
+++ ui/src/__tests__/NoResultsMessage.test.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { NoResultsMessage } from '../NoResultsMessage';
+
+describe('NoResultsMessage', () => {
+  it('renders the message when show is true', () => {
+    render(<NoResultsMessage show={true} />);
+
+    expect(screen.getByText('No results found. Please refine your query!')).toBeInTheDocument();
+  });
+
+  it('does not render the message when show is false', () => {
+    render(<NoResultsMessage show={false} />);
+
+    expect(screen.queryByText('No results found. Please refine your query!')).not.toBeInTheDocument();
+  });
+});
