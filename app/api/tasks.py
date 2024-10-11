698b86431043bba53a8b81ef6be4304ff8efe687
Ioannis Tsanaktsidis <v-itsanaktsidis@expediagroup.com>
2024-10-11 14:33:31 +0200

    apply pagination

--- app/api/tasks.py
+++ app/api/tasks.py
@@ -3,25 +3,45 @@ import requests
 from .models import Literature
 from datetime import datetime
 
+THRESHOLD = 40
+
 @shared_task
 def harvest_hep_data():
     print("Running harvest_hep_data task")
     url = "https://inspirehep.net/api/literature"
     params = {
-        'size': 40,
+        'size': 10,
         'sort': 'mostrecent'
     }
-    response = requests.get(url, params=params, verify=False)
+    literature_results = []
+    next_url = url
 
-    if response.status_code == 200:
-        literature_result = response.json().get('hits', {}).get('hits', [])
-        for literature in literature_result:
+    while next_url and len(literature_results) < THRESHOLD:
+        try:
+            response = requests.get(next_url, params=params, verify=False)
+            response.raise_for_status()
+
+            data = response.json()
+            literature_result = data.get('hits', {}).get('hits', [])
+            literature_results.extend(literature_result)
+
+            next_url = data.get('links', {}).get('next')
+            params = {}
+
+        except requests.exceptions.RequestException as e:
+            print(f"Failed to fetch data: {e}")
+            # return Response({"error": "Failed to fetch data"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        except ValueError as e:
+            print(f"Failed to fetch data: {e}")
+            # return Response({"error": "Invalid JSON response"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    for literature in literature_results:
             literature_data = literature.get('metadata', {})
 
             title = literature_data.get('titles', [{}])[0].get('title', '')
             abstract = literature_data.get('abstracts', [{}])[0].get('value', '')
             publication_date_str = literature_data.get('imprints', [{}])[0].get('date', '')
-
+            print(f"Processing paper: {title} , {publication_date_str} , {abstract.strip()[:50]}...")
             try:
                 publication_date = datetime.strptime(publication_date_str, "%Y-%m-%d").date()
             except (ValueError, TypeError):
