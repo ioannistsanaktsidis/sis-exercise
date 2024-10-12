7d250c77f024de6c8c4b7c0f814c3631336009c7
Ioannis Tsanaktsidis <v-itsanaktsidis@expediagroup.com>
2024-10-12 19:24:51 +0200

    format

--- ui/src/__tests__/NoResultsMessage.test.tsx
+++ ui/src/__tests__/NoResultsMessage.test.tsx
@@ -1,18 +1,22 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import { NoResultsMessage } from '../components/NoResultsMessage';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import { NoResultsMessage } from "../components/NoResultsMessage";
 
-describe('NoResultsMessage', () => {
-  it('renders the message when show is true', () => {
+describe("NoResultsMessage", () => {
+  it("renders the message when show is true", () => {
     render(<NoResultsMessage show={true} />);
 
-    expect(screen.getByText('No results found. Please refine your query!')).toBeInTheDocument();
+    expect(
+      screen.getByText("No results found. Please refine your query!")
+    ).toBeInTheDocument();
   });
 
-  it('does not render the message when show is false', () => {
+  it("does not render the message when show is false", () => {
     render(<NoResultsMessage show={false} />);
 
-    expect(screen.queryByText('No results found. Please refine your query!')).not.toBeInTheDocument();
+    expect(
+      screen.queryByText("No results found. Please refine your query!")
+    ).not.toBeInTheDocument();
   });
 });
